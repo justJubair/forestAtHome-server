68f596b49d6f5bc86a84858a247e2d282170f95a
Jubair <jubair.ahmed2838@gmail.com>
2024-07-13 14:50:35 +0600

    [Add]: search and sorting api

--- index.ts
+++ index.ts
@@ -41,7 +41,14 @@ app.post("/api/v1/product", async (req, res) => {
 
 app.get("/api/v1/product", async (req, res) => {
   try {
-    const result = await ProductModel.find({ isDeleted: false });
+    const filter = req?.query;
+    const query = {
+      title: { $regex: filter.search || "", $options: "i" },
+    };
+
+    const result = await ProductModel.find(query).sort({
+      price: filter.sort === "asc" ? 1 : -1,
+    });
     res.status(200).json({
       success: true,
       message: "Products Retrieved Successfully",
